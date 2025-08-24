38f35b2b1404a5d2300f13ca86ea74325a3e2b3f
Mehmetkerem <hakankerem8@gmail.com>
2025-08-24 21:46:52 +0300

    feat(order): add saveOrder endpoint in OrderController

--- src/main/java/com/mehmetkerem/controller/impl/RestOrderControllerImpl.java
+++ src/main/java/com/mehmetkerem/controller/impl/RestOrderControllerImpl.java
@@ -1,8 +1,28 @@
 package com.mehmetkerem.controller.impl;
 
 import com.mehmetkerem.controller.IRestOrderController;
-import org.springframework.web.bind.annotation.RestController;
+import com.mehmetkerem.dto.request.OrderRequest;
+import com.mehmetkerem.dto.response.CartResponse;
+import com.mehmetkerem.dto.response.OrderResponse;
+import com.mehmetkerem.service.IOrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1/order")
 public class RestOrderControllerImpl implements IRestOrderController {
+
+    private final IOrderService orderService;
+
+    public RestOrderControllerImpl(IOrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(
+            @RequestParam String userId, @RequestBody @Valid OrderRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.saveOrder(userId,request));
+    }
 }
