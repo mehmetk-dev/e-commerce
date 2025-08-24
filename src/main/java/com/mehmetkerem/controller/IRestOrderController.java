fbc87ce5e1a584c666bb73e506d69821fa82bb31
Mehmetkerem <hakankerem8@gmail.com>
2025-08-24 22:20:57 +0300

    feat(payment): update  controller endpoints with ResponseEntity.

--- src/main/java/com/mehmetkerem/controller/IRestOrderController.java
+++ src/main/java/com/mehmetkerem/controller/IRestOrderController.java
@@ -1,4 +1,23 @@
 package com.mehmetkerem.controller;
 
+import com.mehmetkerem.dto.request.OrderRequest;
+import com.mehmetkerem.dto.response.OrderResponse;
+import com.mehmetkerem.enums.OrderStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface IRestOrderController {
+
+    ResponseEntity<OrderResponse> saveOrder(String userId, OrderRequest request);
+
+    ResponseEntity<OrderResponse> getOrderResponseById(String orderId);
+
+    ResponseEntity<List<OrderResponse>> getOrdersByUser(String userId);
+
+    ResponseEntity<List<OrderResponse>> getAllOrders();
+
+    ResponseEntity<OrderResponse> updateOrderStatus(String orderId, OrderStatus newStatus);
+
+    ResponseEntity<String> deleteOrder(String orderId);
 }
--- src/main/java/com/mehmetkerem/controller/impl/RestOrderControllerImpl.java
+++ src/main/java/com/mehmetkerem/controller/impl/RestOrderControllerImpl.java
@@ -4,11 +4,14 @@ import com.mehmetkerem.controller.IRestOrderController;
 import com.mehmetkerem.dto.request.OrderRequest;
 import com.mehmetkerem.dto.response.CartResponse;
 import com.mehmetkerem.dto.response.OrderResponse;
+import com.mehmetkerem.enums.OrderStatus;
 import com.mehmetkerem.service.IOrderService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/order")
@@ -20,9 +23,41 @@ public class RestOrderControllerImpl implements IRestOrderController {
         this.orderService = orderService;
     }
 
-    @PostMapping
-    public ResponseEntity<OrderResponse> createOrder(
+    @Override
+    @PostMapping("/save")
+    public ResponseEntity<OrderResponse> saveOrder(
             @RequestParam String userId, @RequestBody @Valid OrderRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.saveOrder(userId,request));
+    }
+
+    @Override
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> getOrderResponseById(@PathVariable String orderId) {
+        return ResponseEntity.ok(orderService.getOrderResponseById(orderId));
+    }
+
+    @Override
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<OrderResponse>> getOrdersByUser(@PathVariable String userId) {
+        return ResponseEntity.ok(orderService.getOrdersByUser(userId));
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
+    }
+
+    @PutMapping("/{orderId}/status")
+    public ResponseEntity<OrderResponse> updateOrderStatus(
+            @PathVariable String orderId,
+            @RequestParam OrderStatus newStatus
+    ) {
+        return ResponseEntity.ok(orderService.updateOrderStatus(orderId, newStatus));
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<String> deleteOrder(@PathVariable String orderId) {
+        return ResponseEntity.ok(orderService.deleteOrder(orderId));
     }
 }
